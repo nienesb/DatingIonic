d0905bcd3677fc98dabb220d10498e069ebb1514
milansteenwinkel <milanosie@live.nl>
2017-06-24 13:30:24 +0200

    Bargraph for Day

--- src/pages/platform-details/platform-details.ts
+++ src/pages/platform-details/platform-details.ts
@@ -20,12 +20,17 @@ import {AddNotePage} from "../notities-pages/add-note/add-note";
 })
 export class PlatformDetailsPage {
   @ViewChild('lineCanvas') lineCanvas;
+  @ViewChild('barCanvas') barCanvas;
   public prognostics;
   public platform = this.navParams.get("platform");
   public selectedDate: any = moment().hour(1).minute(0).toISOString();
   public endDate: any = moment(this.selectedDate).add(1, 'days').toISOString();
   public scoreArray: Array<number> = new Array<number>();
   public labelArray: Array<string> = new Array<string>();
+
+  public statsArray: Array<number> = new Array<number>();
+  public barLabelArray: Array<String> = new Array<String>();
+
   public dayOrMonth = 'day';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private statsProvider: StatisticProvider, public toastCtrl: ToastController) { }
@@ -45,6 +50,7 @@ export class PlatformDetailsPage {
 
   ionViewDidLoad() {
     this.createCanvas(this.scoreArray, this.labelArray);
+    this.createBarCanvas(this.statsArray, this.barLabelArray);
     this.getPrognostics(this.platform.id, this.selectedDate, this.endDate)
   }
 
@@ -57,11 +63,8 @@ export class PlatformDetailsPage {
           this.labelArray = [];
 
           if(data.length > 0) {
-            for (let prognostic of this.prognostics) {
-              this.scoreArray.push(parseInt(prognostic.score));
-              this.labelArray.push(prognostic.date.toString().substr(11, 2) + "U");
-            }
-            this.addData(this.lineCanvas, this.labelArray, this.scoreArray);
+            this.updateLineChartForDailyPrognostics();
+            this.calculateTopsAndDowns(data);
           } else {
             this.showToast('Geen data gevonden');
           }
@@ -76,13 +79,12 @@ export class PlatformDetailsPage {
           this.prognostics = data;
           this.scoreArray = [];
           this.labelArray = [];
+          this.statsArray = [];
+          this.barLabelArray = [];
 
           if(data.length > 0) {
-            for (let prognostic of this.prognostics) {
-              this.scoreArray.push(parseInt(prognostic.averageProceedsDailyPercentage));
-              this.labelArray.push(moment(prognostic.date).format('MMM Do'));
-            }
-            this.addData(this.lineCanvas, this.labelArray, this.scoreArray);
+            this.updateLineChartForMonthlyPrognostics();
+            this.calculateTopsAndDowns(data);
           } else {
             this.showToast('Geen data gevonden');
           }
@@ -94,7 +96,24 @@ export class PlatformDetailsPage {
     }
   }
 
+  private updateLineChartForDailyPrognostics() {
+    for (let prognostic of this.prognostics) {
+      this.scoreArray.push(parseInt(prognostic.score));
+      this.labelArray.push(prognostic.date.toString().substr(11, 2) + "U");
+    }
+    this.addData(this.lineCanvas, this.labelArray, this.scoreArray);
+  }
+
+  private updateLineChartForMonthlyPrognostics() {
+    for (let prognostic of this.prognostics) {
+      this.scoreArray.push(parseInt(prognostic.averageProceedsDailyPercentage));
+      this.labelArray.push(moment(prognostic.date).format('MMM Do'));
+    }
+    this.addData(this.lineCanvas, this.labelArray, this.scoreArray);
+  }
+
   private addData(chart, label, data) {
+
     chart.data.labels = label;
     chart.data.datasets[0].data = data;
     chart.update();
@@ -114,6 +133,42 @@ export class PlatformDetailsPage {
     });
   }
 
+  private createBarCanvas(statsArray, labelArray) {
+    this.barCanvas = new Chart(this.barCanvas.nativeElement, {
+
+      type: 'bar',
+      data: {
+        labels: labelArray,
+        datasets: [{
+          label: this.platform.name,
+          data: statsArray,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
+  }
+
   private showToast(messageString: string) {
     let toast = this.toastCtrl.create({
       message: messageString,
@@ -125,5 +180,40 @@ export class PlatformDetailsPage {
 
   openNotes(platform) {
     this.navCtrl.push(AddNotePage, {platform: platform});
+  }
+
+  private calculateTopsAndDowns(prognostics) {
+    if(this.dayOrMonth == 'day') {
+      this.createBarGraphForDay(prognostics);
+    }
+  }
+
+  private createBarGraphForDay(prognostics) {
+    let highestProfitPercentage = null;
+    let lowestProfitPercentage = null;
+    let highestScore = null;
+    let lowestScore = null;
+
+    for (let prognostic of prognostics) {
+      if (parseInt(prognostic.profitPercentage) < lowestProfitPercentage || lowestProfitPercentage == null) {
+        lowestProfitPercentage = parseInt(prognostic.profitPercentage);
+      }
+      if (parseInt(prognostic.profitPercentage) > highestProfitPercentage || highestProfitPercentage == null) {
+        highestProfitPercentage = parseInt(prognostic.profitPercentage);
+      }
+      if (parseInt(prognostic.score) < lowestScore || lowestScore == null) {
+        lowestScore = parseInt(prognostic.score);
+      }
+      if (parseInt(prognostic.score) > highestScore || highestScore == null) {
+        highestScore = parseInt(prognostic.score);
+      }
+    }
+    this.statsArray = [];
+    this.statsArray.push(lowestProfitPercentage);
+    this.statsArray.push(highestProfitPercentage);
+    this.statsArray.push(lowestScore);
+    this.statsArray.push(highestScore);
+    this.barLabelArray = ['L. Profit', 'H. Profit', 'L. Score', 'H. Score'];
+    this.addData(this.barCanvas, this.barLabelArray, this.statsArray);
   }
 }
